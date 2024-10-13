d5f31fbd88a3e58c597f891d65430fb9a4aefc8e
Mike Walters <mike@flomp.net>
2024-10-13 21:11:56 +0100

    Add vendor request 2 handler

--- ecal-emulate.py
+++ ecal-emulate.py
@@ -34,6 +34,13 @@ class ECalDevice(USBDevice):
                 def handle_data_received(self, data):
                     print(f"Received data: {data}")
 
+    @vendor_request_handler(number=2, direction=USBDirection.OUT)
+    @to_device
+    def handle_control_request_2(self, request):
+        print(request)
+        request.ack()
+
+
     @vendor_request_handler(number=4, direction=USBDirection.OUT)
     @to_device
     def handle_control_request_4(self, request):
